63be0718c43a203611af8512883015fd02542ca3
TirabosqueDesiree <TirabosqueDesiree@users.noreply.github.com>
2021-01-05 09:52:22 -0300

    PracticaMVC C#
    MVC

---
+++ webPrueba1/webPrueba1/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webPrueba1.Models;
+
+namespace webPrueba1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuario modelo)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Principal", "Home");
+            }
+            else
+            {
+                return View(modelo);
+            }
+            
+        }
+    }
+}
---
+++ webPrueba1/webPrueba1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webPrueba1.MvcApplication" Language="C#" %>
