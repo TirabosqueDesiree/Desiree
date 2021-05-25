cc43f8a7ea7a014432144ac5b84004e74b0ee4ff
111598-TirabosqueDesiree-2W50 <63618460+TirabosqueDesiree@users.noreply.github.com>
2021-05-25 17:59:44 -0300

    TesisWeb 1.2
    agregue imagen a Carga - Producto

---
+++ TesisWeb/TesisWeb/TesisWeb/Filters/VerificaSession.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesisWeb.Controllers;
+using TesisWeb.Models;
+
+namespace TesisWeb.Filters
+{
+    public class VerificaSession : ActionFilterAttribute
+    {
+        private Usuarios oUsuario;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+
+                oUsuario = (Usuarios)HttpContext.Current.Session["User"];
+                if (oUsuario == null)
+                {
+                    if (filterContext.Controller is AccesoController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("~/Acceso/Login");
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                filterContext.Result = new RedirectResult("~/Acceso/Login");
+            }
+
+        }
+    }
+}
---
+++ TesisWeb/TesisWeb/TesisWeb/Models/clasesProducto/Oferta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesisWeb.Models.clasesProducto
+{
+    public class Oferta
+    {
+        public int IdOferta { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
